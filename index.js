ddea956801f25564af3c99e2ab6949fa6bd31fde
Vince Garcia <vinceryangarcia@gmail.com>
2021-03-11 11:08:05 -0800

    test

--- index.js
+++ index.js
@@ -5,29 +5,28 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
-
-// tells express where static assets are found
-// joins absolute path to the project to path to static assets
 server.use(express.static(path.join(__dirname, 'client/build')))
 
 console.log(process.env.NODE_ENV)
 
-/// on Heroku machine, an env variable is called "NODE_ENV" -> "production"
+// on Heroku machine, an env variable is called "NODE_ENV" -> "production"
 if (process.env.NODE_ENV === 'development') {
   const cors = require('cors')
   server.use(cors())
 }
 
+// our API comes earlier in the pipeline
 server.get('/api/hello', (req, res) => {
-    res.json({ message: 'hello'})
+  res.json({ message: 'i love you all' })
 })
 
+// catch-all that just sends back index.html
 server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
+  console.log(`listening on ${PORT}`)
 })
